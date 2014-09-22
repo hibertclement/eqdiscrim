907bcf9edac1170dd2dc2cc3a618d2fcd42207de
Alessia Maggi <alessia.maggi@unistra.fr>
2014-09-22 17:08:54 +0200

    Added time parsing to sihex io routine

--- eqdiscrim/cat_io/sihex_io.py
+++ eqdiscrim/cat_io/sihex_io.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
+from dateutil import tz
+
+utc = tz.gettz('UTC')
 
 
 # files to be read (static for now - ugly but efficient - fix later)
@@ -7,23 +11,58 @@ sihex_xls = '../static_catalogs/SIHEXV2-inout-final.xlsx'
 sihex_txt = '../static_catalogs/SIHEXV2-catalogue-final.txt'
 
 
-def read_sihex_xls():
+def read_sihex_xls(inout=True):
 
     # read the excel file into pandas data frames
     eq_in = pd.read_excel(sihex_xls, sheetname=0)
-    eq_out = pd.read_excel(sihex_xls, sheetname=1)
-    # eq_out_felt = pd.read_excel(sihex_xls, sheetname=2)
+    if inout:
+        eq_out = pd.read_excel(sihex_xls, sheetname=1)
+
+    # date-time reading
+    DT_names = list(["DATE", "HEURE"])
+    DT_in = eq_in[DT_names].values
+    if inout:
+        DT_out = eq_out[DT_names].values
+        DT = np.vstack((DT_in, DT_out))
+    else:
+        DT = DT_in
+
+    # date-time parsing into a single otime object
+    nev, nd = DT.shape
+    otime = np.empty(nev, dtype=object)
+    for i in xrange(nev):
+        # Date is encoded as a pandas imestamp
+        year = DT[i, 0].year
+        month = DT[i, 0].month
+        day = DT[i, 0].day
+        # time is encoded as a string
+        time_parts = DT[i, 1].split(':')
+        hour = np.int(time_parts[0])
+        minute = np.int(time_parts[1])
+        seconds = np.int(np.floor(np.float(time_parts[2])))
+        microseconds = np.int((np.float(time_parts[2]) - seconds) * 1e6)
+        # make a datetime object by merging date and time
+        otime[i] = datetime(year, month, day, hour, minute, seconds,
+                            microseconds,utc)
 
     # transform the pandas format to numpy format for later use in sklearn
-    names = list(["ID", "DATE", "HEURE", "LAT", "LON", "PROF", "Mw", "AUTEUR"])
+    names = list(["ID", "LAT", "LON", "PROF", "Mw", "AUTEUR"])
     X_in = eq_in[names].values
-    X_out = eq_out[names].values
-    X = np.vstack((X_in, X_out))
-    n_ev, n_names = X.shape
+    if inout:
+        X_out = eq_out[names].values
+        X_tmp = np.vstack((X_in, X_out)) 
+    else:
+        X_tmp = X_in
+
+    # add the otime between "ID" and "LAT"
+    nnames = len(names)
+    X = np.hstack((X_tmp[:, 0].reshape(nev, 1), otime.reshape(nev, 1),
+                   X_tmp[:, 1:].reshape(nev, nnames-1)))
+    names.insert(1, "OTIME")
 
     # the current data file does not contain "ke" attributes
     # all the events are "ke"
-    y = np.empty(n_ev, dtype='S2')
+    y = np.empty(nev, dtype='S2')
     y[...] = 'ke'
 
     return X, y, names
--- eqdiscrim/test_io.py
+++ eqdiscrim/test_io.py
@@ -1,4 +1,10 @@
 import unittest
+import numpy as np
+from cat_io.sihex_io import read_sihex_xls
+from datetime import datetime, timedelta
+from dateutil import tz
+
+utc = tz.gettz('UTC')
 
 
 def suite():
@@ -10,15 +16,17 @@ def suite():
 class IoReadTests(unittest.TestCase):
 
     def test_read_sihex_xls(self):
-        from cat_io.sihex_io import read_sihex_xls
 
         X, y, names = read_sihex_xls()
 
         nev, natt = X.shape
         self.assertEqual(nev, 50585)
-        self.assertEqual(natt, 8)
+        self.assertEqual(natt, 7)
         self.assertEqual(y[0], 'ke')
-        self.assertEqual(names[1], 'DATE')
+        self.assertEqual(names[1], 'OTIME')
+
+        test_otime = datetime(1962, 3, 7, 1, 25, 39, np.int(0.7 * 1e6), utc)
+        self.assertEqual(test_otime - X[3, 1], timedelta(0))
 
 if __name__ == '__main__':
 
