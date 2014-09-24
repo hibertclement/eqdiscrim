27fcacd97b344d484f0c091e1b61c03484822227
Alessia Maggi <alessia.maggi@unistra.fr>
2014-09-24 11:22:29 +0200

    Added plotting of year and hour histograms

--- eqdiscrim/data_exploration.py
+++ eqdiscrim/data_exploration.py
@@ -1,15 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from cat_io.sihex_io import read_sihex_xls
 from preproc import latlon_to_xy
 from sklearn.preprocessing import StandardScaler
 from sklearn import cluster
 from graphics.graphics_2D import plot_2D_cluster_scatter
 from graphics.graphics_2D import plot_att_hist_by_label
+from dateutil import tz
+
+
+to_zone = tz.gettz('Europe/Paris')
 
 
 # read catalog
 Xlatlon, y, names_latlon = read_sihex_xls()
+
+nev, natt = Xlatlon.shape
 
 # extract the x and y coordinates
 print Xlatlon.shape, names_latlon
@@ -30,10 +36,16 @@ X_m = X[:, imag]
 iauth = 6
 X_auth = X[:, iauth]
 
+# extract local time
+i_time = 1
+X_loctime = np.array([t.astimezone(to_zone) for t in X[:, i_time]])
+
 
 # scale geographical coordinates
 scaler = StandardScaler().fit(X_xy)
 X_xy = scaler.transform(X_xy)
+X_year = np.array([t.year for t in X_loctime])
+X_hour = np.array([t.hour for t in X_loctime])
 
 
 # create a cluster-izer
@@ -51,16 +63,36 @@ plot_2D_cluster_scatter(X_xy, X_auth,
                         'clusters_by_author.png')
 
 # plot depth and magnitude as a function of author
-depth_range=(-3, 60)
-plot_att_hist_by_label(X_d, X_auth, depth_range, 'Depth (km)',
+nbins = 20
+depth_range=(-3, 40)
+plot_att_hist_by_label(X_d, X_auth, depth_range, nbins, 'Depth (km)',
                        'depth_pdf_by_author.png')
 mag_range=(0, 6)
-plot_att_hist_by_label(X_m, X_auth, mag_range, 'Mw', 'mag_pdf_by_author.png')
+plot_att_hist_by_label(X_m, X_auth, mag_range, nbins, 'Mw',
+                       'mag_pdf_by_author.png')
 
 # plot depth and magnitude as a function of cluster
-depth_range=(-3, 60)
-plot_att_hist_by_label(X_d, clf.labels_, depth_range, 'Depth (km)',
+depth_range=(-3, 40)
+plot_att_hist_by_label(X_d, clf.labels_, depth_range, nbins, 'Depth (km)',
                        'depth_pdf_by_cluster.png')
 mag_range=(0, 6)
-plot_att_hist_by_label(X_m, clf.labels_, mag_range, 'Mw',
+plot_att_hist_by_label(X_m, clf.labels_, mag_range, nbins, 'Mw',
                        'mag_pdf_by_cluster.png')
+
+# plot year as a function of author and cluster
+nbins = 24
+time_range = (1962, 2010)
+plot_att_hist_by_label(X_year, X_auth, time_range, nbins, 'Year',
+                       'year_pdf_by_author.png')
+plot_att_hist_by_label(X_year, clf.labels_, time_range, nbins, 'Year',
+                       'year_pdf_by_cluster.png')
+
+# plot hour as a function of author and cluster
+nbins = 24
+time_range = (0, 23)
+plot_att_hist_by_label(X_hour, X_auth, time_range, nbins, 'Local hour',
+                       'hour_pdf_by_author.png')
+plot_att_hist_by_label(X_hour, clf.labels_, time_range, nbins, 'Local hour',
+                       'hour_pdf_by_cluster.png')
+
+
--- eqdiscrim/graphics/graphics_2D.py
+++ eqdiscrim/graphics/graphics_2D.py
@@ -22,12 +22,12 @@ def plot_2D_cluster_scatter(X, labels, at_name, filename):
         x = X[:, 0][labels == cname]
         y = X[:, 1][labels == cname]
         plt.scatter(x, y, c=colors[i])
-    plt.legend(clust_names)
+    plt.legend(clust_names, bbox_to_anchor=(1.05, 1.05))
     plt.xlabel(at_name[0])
     plt.ylabel(at_name[1])
     plt.savefig(filename)
 
-def plot_att_hist_by_label(X_d, X_auth, att_range, att_name, filename):
+def plot_att_hist_by_label(X_d, X_auth, att_range, nbins, att_name, filename):
     """
     Plots attribute histograms by author
     """
@@ -39,7 +39,7 @@ def plot_att_hist_by_label(X_d, X_auth, att_range, att_name, filename):
 
     # first plot - all together
     plt.sca(axes[0])
-    n, bins, patches = plt.hist(X_d, 20, range=att_range, normed=1,
+    n, bins, patches = plt.hist(X_d, nbins, range=att_range, normed=1,
                                 histtype='step', color='black')
     plt.xlabel(att_name)
     plt.ylabel('Probability density')
@@ -53,9 +53,9 @@ def plot_att_hist_by_label(X_d, X_auth, att_range, att_name, filename):
     for i in xrange(n_auth):
         author = u_auth[i]
         d = X_d[:][X_auth == author]
-        n, bins, patches = plt.hist(d, 20, range=att_range, normed=1,
+        n, bins, patches = plt.hist(d, nbins, range=att_range, normed=1,
                                     histtype='step', color=colors[i])
-    plt.legend(u_auth)
+    plt.legend(u_auth, bbox_to_anchor=(1.3, 1.05))
     plt.xlabel(att_name)
     plt.ylabel('Probability density')
 
