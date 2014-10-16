208f0b091ad60e0ad235a1c413bd0abd3e3af6f9
Alessia Maggi <alessia.maggi@unistra.fr>
2014-10-16 13:36:27 +0200

    Wrote input for time_based stations
    Not including dates for now

--- eqdiscrim/cat_io/renass_io.py
+++ eqdiscrim/cat_io/renass_io.py
@@ -4,6 +4,7 @@ import numpy as np
 
 # files to be read (static for now - ugly but efficient)
 renass_txt = '../static_catalogs/RENASS_stations_2014.txt'
+stations_csv = '../static_catalogs/stations_fr.csv'
 
 def read_renass():
 
@@ -14,3 +15,15 @@ def read_renass():
     stations = s_pd[names].values
 
     return stations, names
+
+def read_stations_fr():
+
+    # read the csv file using pandas
+    s_pd = pd.read_csv(stations_csv)
+
+    names = list(["NOM", "LATITUDE", "LONGITUDE", "ELEV"])
+    names_DT = list(["DATEDEB", "HDEB", "DATEFIN", "HFIN"])
+
+    stations = s_pd[names].values
+    return stations, names
+
--- eqdiscrim/test_io.py
+++ eqdiscrim/test_io.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from cat_io.sihex_io import read_sihex_xls
-from cat_io.renass_io import read_renass
+from cat_io.renass_io import read_renass, read_stations_fr
 from datetime import datetime, timedelta
 from dateutil import tz
 
@@ -12,6 +12,7 @@ def suite():
     suite = unittest.TestSuite()
     suite.addTest(IoReadTests('test_read_sihex_xls'))
     suite.addTest(IoReadTests('test_read_renass'))
+    suite.addTest(IoReadTests('test_read_stations_fr'))
     return suite
 
 
@@ -37,6 +38,14 @@ class IoReadTests(unittest.TestCase):
         self.assertEqual(nsta, 81)
         self.assertEqual(natt, 4)
         self.assertEqual(stations[1, 1], 43.0858)
+
+    def test_read_stations_fr(self):
+        
+        stations, names = read_stations_fr()
+        nsta, natt = stations.shape
+        self.assertEqual(nsta, 306)
+        self.assertEqual(natt, 4)
+        self.assertEqual(stations[1, 1], 47.268)
 
 if __name__ == '__main__':
 
