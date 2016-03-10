2d2b6b94819f399063ed41a22fb140f6e74df794
Alessia Maggi <alessia.maggi@unistra.fr>
2016-03-10 19:14:53 +0100

    Att 23 : filter problems

--- Piton_de_la_Fournaise_CH/attributes.py
+++ Piton_de_la_Fournaise_CH/attributes.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import hilbert, lfilter, filtfilt, find_peaks_cwt
+from scipy.signal import hilbert, lfilter, filtfilt, find_peaks_cwt, butter
 from scipy.stats import kurtosis, skew
 
 NATT = 61
@@ -17,9 +17,10 @@ def calculate_all_attributes(obspy_stream):
 
     TesMEAN, TesMEDIAN, TesSTD = get_TesStuff(env)
     RappMaxMean, RappMaxMedian = get_RappMaxStuff(TesMEAN, TesMEDIAN)
-    AsDec = get_AsDec(obspy_stream, env)
+    AsDec, RmsDecAmpEnv = get_AsDec(obspy_stream, env)
     KurtoEnv, KurtoSig, SkewnessEnv, SkewnessSig = get_KurtoSkewStuff(obspy_stream, env)
     CorPeakNumber, INT1, INT2, INT_RATIO = get_CorrStuff(obspy_stream)
+    ES, KurtoF = get_freq_band_stuff(obspy_stream)
 
     all_attributes[0, 0] = np.mean(duration(obspy_stream))
     all_attributes[0, 1] = np.mean(RappMaxMean)
@@ -33,6 +34,17 @@ def calculate_all_attributes(obspy_stream):
     all_attributes[0, 9] = np.mean(INT1)
     all_attributes[0, 10] = np.mean(INT2)
     all_attributes[0, 11] = np.mean(INT_RATIO)
+    all_attributes[0, 12] = np.mean(ES[:, 0])
+    all_attributes[0, 13] = np.mean(ES[:, 1])
+    all_attributes[0, 14] = np.mean(ES[:, 2])
+    all_attributes[0, 15] = np.mean(ES[:, 3])
+    all_attributes[0, 16] = np.mean(ES[:, 4])
+    all_attributes[0, 17] = np.mean(KurtoF[:, 0])
+    all_attributes[0, 18] = np.mean(KurtoF[:, 1])
+    all_attributes[0, 19] = np.mean(KurtoF[:, 2])
+    all_attributes[0, 20] = np.mean(KurtoF[:, 3])
+    all_attributes[0, 21] = np.mean(KurtoF[:, 4])
+    all_attributes[0, 22] = np.mean(RmsDecAmpEnv)
 
 
     return all_attributes
@@ -102,13 +114,19 @@ def get_AsDec(st, env):
     ntr = len(st)
     
     AsDec = np.empty(ntr, dtype=float)
+    RmsDecAmpEnv = np.empty(ntr, dtype=float)
 
     for i in xrange(ntr):
         smooth_env = lfilter(strong_filter, 1, env[i])
         imax = np.argmax(smooth_env)
         AsDec[i] = (imax+1) / float(len(st[i].data) - (imax+1))
+        dec = st[i].data[imax:]
+        lendec = len(dec)
+        # I don't know what this is really, but it does not look like an RMS !!
+        RmsDecAmpEnv[i] = np.abs(np.mean(np.abs(hilbert(dec / np.max(st[i].data))) -\
+            (1 - ( (1 / float(lendec)) * (np.arange(lendec)+1) )))) 
     
-    return AsDec
+    return AsDec, RmsDecAmpEnv
 
 def get_KurtoSkewStuff(st, env):
 
@@ -179,3 +197,30 @@ def get_CorrStuff(st):
 
 
     return CorPeakNumber, INT1, INT2, INT_RATIO
+
+def get_freq_band_stuff(st):
+
+    sps = st[0].stats.sampling_rate
+    NyF = sps / 2.
+
+    FFI = np.array([0.1, 1, 3, 10, 20])
+    FFE = np.array([1, 3, 10, 20, 0.99*NyF])
+
+    ntr = len(st)
+    nf = len(FFI)
+
+    ES = np.empty((ntr, nf), dtype=float)
+    KurtoF = np.empty((ntr, nf), dtype=float)
+
+    for i in xrange(ntr):
+        for j in xrange(nf):
+            #import pdb; pdb.set_trace()
+            Fb, Fa = butter(2, [FFI[j]/NyF, FFE[j]/NyF], 'bandpass')
+            tr = st[i].copy()
+            #data_filt = filtfilt(Fb, Fa, tr.data)  # This filter is broken !!
+            data_filt = tr.filter('bandpass', freqmin=FFI[j], freqmax=FFE[j], corners=2, zerophase=True)
+            # this integral is also bogus...
+            ES[i, j] = np.log10(np.trapz(np.abs(hilbert(data_filt))))
+            KurtoF[i, j] = kurtosis(data_filt, fisher=False)
+            
+    return ES, KurtoF
--- Piton_de_la_Fournaise_CH/test_attribute_calculation.py
+++ Piton_de_la_Fournaise_CH/test_attribute_calculation.py
@@ -2,7 +2,9 @@ import unittest, os
 import numpy as np
 import pandas as pd
 from obspy.core import UTCDateTime, read
+from obspy.signal.filter import bandpass
 from scipy.io import loadmat
+from scipy.signal import butter, filtfilt
 
 import data_io as io
 import attributes as at
@@ -13,7 +15,12 @@ def suite():
     suite = unittest.TestSuite()
     suite.addTest(IOTests('test_read_catalog'))
     suite.addTest(IOTests('test_read_and_cut_events'))
-    suite.addTest(IOTests('test_read_mat_file'))
+    suite.addTest(AttributeTests('test_filter_coefs'))
+    suite.addTest(AttributeTests('test_filtering_data'))
+    suite.addTest(AttributeTests('test_attribute_values_0_11'))
+    suite.addTest(AttributeTests('test_attribute_values_12_16'))
+    suite.addTest(AttributeTests('test_attribute_values_17_21'))
+    suite.addTest(AttributeTests('test_attribute_values_22'))
 
     return suite
 
@@ -48,25 +55,83 @@ class IOTests(unittest.TestCase):
         self.assertAlmostEqual(tr.stats.starttime - onset, 0.0, 2)
         self.assertAlmostEqual(tr.stats.endtime - endtime, 0.0, 2)
 
-    def test_read_mat_file(self):
 
-        # load attributes for first event
+class AttributeTests(unittest.TestCase):
+
+    def test_filter_coefs(self):
+
+        mat_fname = os.path.join(datadir, 'filter_coef.mat')
+        mat = loadmat(mat_fname)
+
+        Fb_mat = mat['Fa']
+        Fa_mat = mat['Fb']
+
+        NyF = 100.0 / 2.0
+        FFI = 0.1
+        FFE = 1.0
+
+        Fb_py, Fa_py = butter(2, [FFI/NyF, FFE/NyF], 'bandpass')
+
+        for i in xrange(len(Fb_py)):
+            self.assertAlmostEqual(Fb_mat[0, i], Fb_py[i])
+        for i in xrange(len(Fa_py)):
+            self.assertAlmostEqual(Fa_mat[0, i], Fa_py[i])
+
+    def test_filtering_data(self):
+
+        mat_fname = os.path.join(datadir, 'filtered_data.mat')
+        mat = loadmat(mat_fname)
+        filt_data_mat = mat['filt_data']
+
+        NyF = 100.0 / 2.0
+        FFI = 0.1
+        FFE = 1.0
+        Fb, Fa = butter(2, [FFI/NyF, FFE/NyF], btype='bandpass')
+
+        st = read(os.path.join(datadir, 'events', 'event_01*HHZ*SAC'))
+        tr = st[0].copy()
+        filt_data_py = filtfilt(Fb, Fa, st[0].data)
+        tr.filter('bandpass', freqmin=FFI, freqmax=FFE, corners=2, zerophase=True)
+
+        self.assertAlmostEqual(len(filt_data_mat), len(tr.data))
+        self.assertAlmostEqual(max(filt_data_mat)[0], max(tr.data))
+        #self.assertAlmostEqual(len(filt_data_mat), len(filt_data_py))
+        #self.assertAlmostEqual(max(filt_data_mat)[0], max(filt_data_py))
+        
+    def setUp(self):
+
+       # load attributes for first event
         mat_fname = os.path.join(datadir, 'event01.mat')
         mat = loadmat(mat_fname)
-        mat_array = mat['att']
+        self.mat_array = mat['att']
 
         # get attributes for first event en python
         st = read(os.path.join(datadir, 'events', 'event_01*SAC'))
 
-        py_array = at.calculate_all_attributes(st)
+        self.py_array = at.calculate_all_attributes(st)
 
-        # start assertions
-        self.assertSequenceEqual(mat_array.shape, py_array.shape)
 
-        nr, nc =  py_array.shape
-        for i in xrange(nc):
-            print i
-            self.assertAlmostEqual(mat_array[0, i], py_array[0, i], 5)
+    def test_attribute_values_0_11(self):
+
+         # start assertions
+        self.assertSequenceEqual(self.mat_array.shape, self.py_array.shape)
+
+        for i in xrange(12):
+            self.assertAlmostEqual(self.mat_array[0, i], self.py_array[0, i], 5)
+
+    def test_attribute_values_12_16(self):
+
+        for i in [12, 13, 14, 15, 16]:
+            self.assertAlmostEqual(self.mat_array[0, i], self.py_array[0, i], 5)
+
+    def test_attribute_values_17_21(self):
+
+        for i in [17, 18, 19, 20, 21]:
+            self.assertAlmostEqual(self.mat_array[0, i], self.py_array[0, i], 5)
+
+    def test_attribute_values_22(self):
+
+        self.assertAlmostEqual(self.mat_array[0, 22], self.py_array[0, 22], 5)
 
 
         
