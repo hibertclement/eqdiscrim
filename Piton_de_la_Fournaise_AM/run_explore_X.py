12e0dcb1a2e464ae316c0f7b22576cd03505ce6d
Alessia Maggi <alessia.maggi@unistra.fr>
2016-06-23 12:52:36 +0000

    Modifications to do test runs

--- Piton_de_la_Fournaise_AM/run_explore_X.py
+++ Piton_de_la_Fournaise_AM/run_explore_X.py
@@ -7,26 +7,29 @@ import matplotlib.pyplot as plt
 import eqdiscrim_io as io
 import eqdiscrim_graphics as gr
 
+# ---------------------
+# SWITCHES
 do_histograms = False
 do_scatterplots = False
 do_timeplots = False
 do_radviz = False
+# ---------------------
 
+# ---------------------
+# Parameters
+station_names = ["RVL"]
+att_dir = "Attributes_old"
 figdir = 'Figures'
-if not os.path.exists(figdir):
-    os.mkdir(figdir)
+color_list = ['b', 'g', 'r', 'y', 'c', 'm', 'k']
+# ---------------------
+
+# ---------------------
+# Code starts here
 
 pd.set_option('mode.use_inf_as_null', True)
 
-# station_names = ["RVL", "FLR", "BOR", "BON", "SNE", "FJS", "CSS", "GPS", "GPN", "FOR"]
-station_names = ["RVL", "BOR"]
-att_dir = "Attributes"
-
-best_atts = list(['RappMaxStd', 'Duration', 'RappMaxMean', 'KurtoEnv',
-                  'RappMaxMedian', 'AsDec'])
- 
-event_types = ["Effondrement", "Sommital", "Local", "Teleseisme", "Onde sonore"]
-color_list = ['b', 'g', 'r', 'y', 'cyan']
+if not os.path.exists(figdir):
+    os.mkdir(figdir)
 
 # make a different plot for eah station
 for sta in station_names:
@@ -39,12 +42,17 @@ for sta in station_names:
         continue
     print "Reading and concatenating %d dataframes" % len(fnames)
     X_df_full = io.read_and_cat_dataframes(fnames)
+    print X_df_full['EVENT_TYPE'].value_counts()
+    print X_df_full[X_df_full['EVENT_TYPE'] == 'Teleseisme'].head()
 
     # drop all lines containing nan
     print "Dropping all rows containing NaN"
     X_df_full.dropna(inplace=True)
 
     all_atts = X_df_full.columns[5:]
+
+    print X_df_full['EVENT_TYPE'].value_counts()
+    event_types = X_df_full['EVENT_TYPE'].unique()
 
     # extract the subsets according to type
     print "Extracting events according to type"
@@ -53,33 +61,31 @@ for sta in station_names:
         df = X_df_full[X_df_full['EVENT_TYPE'] == evtype]
         df_list.append(df)
 
-    print X_df_full['EVENT_TYPE'].value_counts()
-
     # histograms
     if do_histograms:
         print "Plotting histograms for %d classes and %d attributes" %\
-              (len(df_list), len(best_atts))
-        gr.plot_histograms(df_list, best_atts, color_list, figdir, sta)
+              (len(df_list), len(all_atts))
+        gr.plot_histograms(df_list, all_atts, color_list, figdir, sta)
 
     # scatter plots
     if do_scatterplots:
         print "Plotting scatter plots for %d classes and %d attributes" %\
-            (len(df_list), len(best_atts))
-        gr.plot_scatterplots(df_list, best_atts, color_list, figdir, sta)
+            (len(df_list), len(all_atts))
+        gr.plot_scatterplots(df_list, all_atts, color_list, figdir, sta)
 
     # time plots
     if do_timeplots:
         print "Plotting timeseries plots for %d classes and %d attributes" %\
-              (len(df_list), len(best_atts))
-        gr.plot_att_timeseries(df_list, best_atts, color_list, figdir, sta)
+              (len(df_list), len(all_atts))
+        gr.plot_att_timeseries(df_list, all_atts, color_list, figdir, sta)
 
     # doing radviz plot
     if do_radviz:
         print "Doing radviz plot"
-        best_atts.append('EVENT_TYPE')
+        all_atts.append('EVENT_TYPE')
         all_df = pd.concat(df_list)
-        df_best = all_df[best_atts]   
+        df_all = all_df[all_atts]   
         plt.figure()
-        pd.tools.plotting.radviz(df_best, 'EVENT_TYPE')
+        pd.tools.plotting.radviz(df_all, 'EVENT_TYPE')
         plt.title("%s - Radviz plot" % sta)
         plt.savefig(os.path.join(figdir, "%s_radviz.png" % sta))
