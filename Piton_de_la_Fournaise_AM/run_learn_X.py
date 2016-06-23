72c0fbfad0eed6306a2bcc0f3a9b70c6730df06f
Alessia Maggi <alessia.maggi@unistra.fr>
2016-06-23 12:52:58 +0000

    Adds conf matrix, learn curves and OVPF scorer

--- Piton_de_la_Fournaise_AM/run_learn_X.py
+++ Piton_de_la_Fournaise_AM/run_learn_X.py
@@ -5,9 +5,30 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import cross_val_score
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn import svm
+from sklearn.cross_validation import cross_val_score, train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, precision_score, recall_score, make_scorer
+from sklearn.learning_curve import learning_curve
 import eqdiscrim_io as io
+import eqdiscrim_graphics as gr
+
+# ----------------
+# make OVPF scorer
+def OVPF_score_func(y, y_pred):
+    non_seismic_precision = precision_score(y, y_pred,
+                                            labels=['Effondrement',
+                                                    'Indetermine',
+                                                    'Onde sonore',
+                                                    'Phase T'],
+                                            average='weighted')
+    seismic_recall = recall_score(y, y_pred, labels=['Sommital', 'Local',
+                                                     'Regional',
+                                                     'Teleseisme',
+                                                     'Profond'],
+                                  average='weighted')
+    return np.mean((non_seismic_precision, seismic_recall))
+OVPF_scorer = make_scorer(OVPF_score_func)
+# ----------------
 
 figdir = 'Figures'
 if not os.path.exists(figdir):
@@ -15,16 +36,17 @@ if not os.path.exists(figdir):
 
 pd.set_option('mode.use_inf_as_null', True)
 
-station_names = ["RVL", "FLR", "BOR", "BON", "SNE", "FJS", "CSS", "GPS", "GPN", "FOR"]
-att_dir = "Attributes"
+#station_names = ["RVL", "BOR"]
+station_names = ["RVL"]
+att_dir = "Attributes_old"
 
-event_types = ["Effondrement", "Sommital", "Local", "Teleseisme", "Onde sonore"]
-num_train = 500
+#event_types = ["Effondrement", "Sommital", "Local", "Teleseisme", "Onde sonore", "Regional", "Profond"]
+event_types = ["Sommital", "Local", "Teleseisme", "Regional", "Profond", "Effondrement", "Onde sonore", "Phase T", "Indetermine"]
 
 for sta in station_names:
     # read attributes
     print "Treating station %s" % sta
-    fnames = glob.glob(os.path.join(att_dir, 'X_%s_*_dataframe.dat' % (sta)))
+    fnames = glob.glob(os.path.join(att_dir, 'X_*_%s_*_dataframe.dat' % (sta)))
     if len(fnames) == 0:
         continue
     print "Reading and concatenating %d dataframes" % len(fnames)
@@ -35,88 +57,46 @@ for sta in station_names:
     X_df_full.dropna(inplace=True)
     print X_df_full['EVENT_TYPE'].value_counts()
 
-    # get the list of attributes we are interested in
-    atts = X_df_full.columns[5:]
-    att_list = list([att for att in atts])
-
-    # extract the subsets according to type
-    print "Extracting events according to type"
+    # only learn the classes in the header
+    print "Extracting chosen classes"
     df_list = []
-    df_indexes_list = []
-    for evtype in event_types:
-        df = X_df_full[X_df_full['EVENT_TYPE'] == evtype]
-        n = len(df)
-        df_indexes = np.random.permutation(df.index.values)[0 : min(num_train, n)]
-        df_list.append(df)
-        df_indexes_list.append(df_indexes)
+    for ev_type in event_types:
+        df_list.append(X_df_full[X_df_full['EVENT_TYPE'] == ev_type])
+    X_df = pd.concat(df_list)
+    print X_df['EVENT_TYPE'].value_counts()
 
-    # put them all together
-    all_train_indexes = np.concatenate(df_indexes_list)
-    all_test_indexes = np.concatenate([df.index.values for df in df_list])
+    # get the list of attributes we are interested in
+    atts = X_df.columns[5:].values
+    X = X_df[atts].values
+    y = X_df['EVENT_TYPE'].values
 
-    # extract the training set
-    train_df = X_df_full.ix[all_train_indexes]
-    print train_df['EVENT_TYPE'].value_counts()
-    X_train = train_df[att_list].values
-    Y_train = train_df['EVENT_TYPE'].values
-
-    test_df = X_df_full.ix[all_test_indexes]
-    X_test = test_df[att_list].values
-    Y_test = test_df['EVENT_TYPE'].values
-
-    # do a first classification
-    max_features = int(np.sqrt(len(att_list)))
+    # use a random forest
+    max_features = int(np.sqrt(len(atts)))
     clf = RandomForestClassifier(n_estimators=30, max_features=max_features)
-    clf = clf.fit(X_train, Y_train)
-    # score on training set
-    Y_pred = clf.predict(X_train)
-    scores = cross_val_score(clf, X_train, Y_train, cv=5)
-    cm = confusion_matrix(Y_train, Y_pred)
-    print "Training accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
-    print cm
-    # score on test set
-    Y_pred = clf.predict(X_test)
-    cm = confusion_matrix(Y_test, Y_pred)
-    print "Testing accuracy: %0.2f" % (accuracy_score(Y_test, Y_pred))
-    print cm
-
-    # print importances
-    importances = clf.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in clf.estimators_], axis=0)
-    indices = np.argsort(importances)[::-1]
-
-    # print("Feature ranking:")
-    # for f in xrange(len(att_list)):
-    #    print("Feature %d : %s (%.2f percent)" % (indices[f], att_list[indices[f]], importances[indices[f]]*100))
     
-    # now do it again but with only best attributes
-    print "Using 10 best features"
-    best_atts = [att_list[indices[i]] for i in xrange(10)]
-    X_train = train_df[best_atts].values
-    X_test = test_df[best_atts].values
+    # print "Learning with a random forest"
+    train_sizes, train_scores, valid_scores = learning_curve(clf, X, y,
+        train_sizes=[300, 600, 1000, 1300, 1600,  2000, 2100, 2200], cv=5, scoring=OVPF_scorer)
+#        train_sizes=[300, 600, 900, 1000, 1200, 1400, 1600, 1800, 1900], cv=5, scoring=OVPF_scorer)
+    gr.plot_learning_curve(train_sizes, train_scores, valid_scores, 'Random Forest at %s' % sta, 'learn_%s.png' % sta)
 
-    clf = RandomForestClassifier(n_estimators = 10)
-    clf = clf.fit(X_train, Y_train)
-    # score on training set
-    Y_pred = clf.predict(X_train)
-    scores = cross_val_score(clf, X_train, Y_train, cv=5)
-    cm = confusion_matrix(Y_train, Y_pred)
-    print "Training accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+    print "Producing confusion matrix"
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    clf_fitted = clf.fit(X_train, y_train)
+    y_pred = clf_fitted.predict(X_test)
+
+    print('Confusion matrix')
+    cm = confusion_matrix(y_test, y_pred)
+    cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    labels = np.unique(y)
     print cm
-    # score on test set
-    Y_pred = clf.predict(X_test)
-    cm = confusion_matrix(Y_test, Y_pred)
-    print "Testing accuracy: %0.2f" % (accuracy_score(Y_test, Y_pred))
-    print cm
+    # gr.plot_confusion_matrix(cm, labels, 'Random Forest at %s' % sta, 'cm_%s.png' % sta)
+    gr.plot_confusion_matrix(cm_norm, labels, 'Random Forest at %s' % sta, 'cm_norm_%s.png' % sta)
+    
+    print('Classification report')
+    cr = classification_report(y_test, y_pred, target_names=labels)
+    print cr
 
-    # print importances
-    importances = clf.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in clf.estimators_], axis=0)
-    indices = np.argsort(importances)[::-1]
-
-    print("Feature ranking:")
-    for f in xrange(len(best_atts)):
-        print("Feature %d : %s (%.2f percent)" % (indices[f], att_list[indices[f]], importances[indices[f]]*100))
-
-
-
+    print('Cross validation scores using OVPF metric')
+    cv_scores = cross_val_score(clf, X, y, scoring=OVPF_scorer, cv=5)
+    print("%.2f (+/-) %.2f" % (np.mean(cv_scores), np.std(cv_scores)))
